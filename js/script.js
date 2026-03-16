1ac585e7dbba8606754e6c856f05044173dedfb6
ikkicuteee <168399842+ikkicuteee@users.noreply.github.com>
2026-03-16 14:29:32 +0800

    Initialization
    uploaded the main dashboard, and mock floor page

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function showRoom(room){
+
+document.getElementById("roomTitle").innerText = room;
+
+document.getElementById("roomInfo").innerText =
+"Active People: 3 | Sensor Status: Online";
+
+}
